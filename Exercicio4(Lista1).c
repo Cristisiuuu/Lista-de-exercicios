e67be2d41aeca8dda4b23b47c44584fe88168a8d
Cristiano Marques Silva <118139423+Cristisiuuu@users.noreply.github.com>
2023-08-30 12:13:17 -0300

    Update Exercicio4(Lista1).c
    Alterei a porte das variaveis

--- Exercicio4(Lista1).c
+++ Exercicio4(Lista1).c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    char inputString[100];
+    char string[100];
     printf("Digite uma string contendo 0's e 1's: ");
-    scanf("%s", inputString);
+    scanf("%s", string);
 
-    int count = 0;
-    for (int i = 0; inputString[i] != '\0'; i++) {
-        if (inputString[i] == '1') {
-            count++;
+    int contagem = 0;
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] == '1') {
+            contagemt++;
         }
     }
 
-    printf("O numero de ocorrencias de '1' na string eh: %d\n", count);
+    printf("O numero de ocorrencias de '1' na string eh: %d\n", contagem);
 
     return 0;
 }
