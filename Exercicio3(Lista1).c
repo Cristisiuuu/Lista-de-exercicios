75b7920c60ae187fbab9915ae59d8050faca7bed
Cristiano Marques Silva <118139423+Cristisiuuu@users.noreply.github.com>
2023-08-30 09:29:45 -0300

    Exercício 3 da Lista 1

---
+++ Exercicio3(Lista1).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char string1[20], string2[20];
+
+    printf("Digite algo na primeira string:\n");
+    gets(string1);
+
+    printf("Digite algo na segunda string:\n");
+    gets(string2);
+
+    int i = 0;
+    int different = 0;
+    //esse while faz a comparação de caractere por caractere;
+    while (string1[i] != '\0' && string2[i] != '\0') {
+        if (string1[i] != string2[i]) {
+            different = 1;
+            break;
+        }
+        i++;
+    }
+
+    if (different || string1[i] != string2[i]) {
+        puts("Diferentes ==> (0)\n");
+    } else {
+        puts("Iguais ==> (1)\n");
+    }
+    
+    return 0;
+}
